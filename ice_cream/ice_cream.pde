f54ca23a0bf649e6ee211bdf142da62d7a5666bb
Tacoalarmclock <league@iMac-2.attlocal.net>
2019-01-28 18:28:25 -0800

    week 3
    rocketship, ages, ice cream banana

---
+++ ice_cream/ice_cream.pde
@@ -0,0 +1,13 @@
+void setup(){
+
+int y = 200;
+size(400, 400);
+
+} 
+
+void draw(){
+for (int i=0;i<120;i = i + 30){
+text("ice cream", 200, i + 180 );
+}
+
+} 
--- src/ages.java
+++ src/ages.java
@@ -1,3 +1,4 @@
+import java.nio.channels.ShutdownChannelGroupException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
@@ -5,39 +6,36 @@ import java.util.Calendar;
 import java.util.Date;
 
 import javax.swing.JOptionPane;
+
 public class ages {
 
-	
-
+	public static void main(String[] args) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println(dateFormat.format(date)+ " is the current time/date"); // 2016/11/16 12:08:43
+		String a = dateFormat.format(date);
+		String year = a.substring(0, 4);
+		String month = a.substring(5, 7);
+		String day = a.substring(8, 9);
+		int Y = Integer.parseInt((year));
+		int M = Integer.parseInt((month));
+				
+		String yInput = JOptionPane.showInputDialog("year of birth (not saved)");
+		String mInput = JOptionPane.showInputDialog("month of birth (not saved)");
 		
-
+		int yInput1 = Integer.parseInt((yInput));
+		int mInput1 = Integer.parseInt((mInput));
 		
-
-		    public static void main(String[] args) {
-		    	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		    	Date date = new Date();
-		    	System.out.println(dateFormat.format(date)); //2016/11/16 12:08:43
-		    	String a = dateFormat.format(date);
-		    	String year = a.substring(0, 3);
-		    	
-		    	Integer.parseInt(dateFormat.format(date));
-		    }
-
+		int yAns = Y - yInput1;
+		int mAns = M - mInput1;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
+		// month to year
+		if (mAns < 0) {
+			mAns = mAns + 12;
+			yAns = yAns - 1;
+		}	
+			System.out.println("You are " + yAns + " years and " + mAns + " months years old!");
+		JOptionPane.showMessageDialog(null, "You are " + yAns + " years and " + mAns + " months years old!");
+	}
 
 }
